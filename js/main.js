9b7c31202caf2301fc4e04f2ce0d1fcad9b94e44
Giorgi Agdgomelashvili <agdgomelashvili18@gmail.com>
2022-12-27 19:42:19 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.bike-slider, .slider-list').slick({
+    arrows: false /* turn off arrows */,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000 /* autoplay 200ms */,
+  });
+});
